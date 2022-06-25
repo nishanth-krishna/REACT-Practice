52a0c8b36d415d629ee7b81ca096a577acdaa15a
nishanth-krishna <krishna.churchmal00@gmail.com>
2022-06-25 19:42:57 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header'
 import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+      <div>
+        <footer className="footerr">
+          <p>THIS IS THE FOOTER</p>
+        </footer>
+      </div>
+    );
+  }
+
+export default Footer;
