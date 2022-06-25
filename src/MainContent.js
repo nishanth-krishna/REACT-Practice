af7d0ca6e2af2ce16eb051a99a84b78c109f3b15
nishanth-krishna <krishna.churchmal00@gmail.com>
2022-06-25 19:32:39 +0530

    first commit

---
+++ src/MainContent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function MainContent() {
+    return (
+      <div>
+        <ul>
+          <li>Was first realeased in 2013</li>
+          <li>Was originally created by John Wick</li>
+          <li>Was first realeased in 2013</li>
+          <li>Was originally created by John Wick</li>
+        </ul>
+      </div>
+    );
+}
+
+export default MainContent;
