f90df667f4608647b59723e0ae98849c7744a02f
nishanth-krishna <krishna.churchmal00@gmail.com>
2022-06-27 11:39:32 +0530

    msg

--- src/MainContent.js
+++ src/MainContent.js
@@ -3,8 +3,9 @@ import React from "react";
 
 function MainContent() {
     return (
+      // <div className="main">
       <div>
-        <ul>
+        <ul className="facts">
           <li>Was first realeased in 2013</li>
           <li>Was originally created by John Wick</li>
           <li>Was first realeased in 2013</li>
